83320e49ff2b0e88cfc24b4a5ac1528427af28be
Alfin123-stack <alfinkarin789@gmail.com>
2024-10-29 23:55:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayOut from "./ui/AppLayOut";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayOut />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:id",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <hader>
+            <Link to="/" >Fast React Pizza</Link>
+        </hader>
+    )
+}
+
+export default Header
